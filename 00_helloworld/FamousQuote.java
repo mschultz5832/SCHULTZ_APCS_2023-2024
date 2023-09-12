a3bf3527c892a68135ca3067941b26924512b567
mschultz5832 <138723184+mschultz5832@users.noreply.github.com>
2023-09-12 09:11:14 -0500

    saying my name

---
+++ 00_helloworld/FamousQuote.java
@@ -0,0 +1,9 @@
+public class FamousQuote {
+    
+    public static void main(String[ ] args)
+    {
+        System.out.println("\" dont let anyone dull your sparkle \"");
+        System.out.println("\" - Mckenna Schultz \"");
+    }
+}
+
---
+++ 00_helloworld/NameConcatenation.java
@@ -0,0 +1,7 @@
+public class NameConcatenation {
+    
+    public static void main(String[] args)
+    {
+        System.out.println("My "  + "name " + "is " +  "Mckenna " +  "Schultz " + ".");
+    }
+ }
