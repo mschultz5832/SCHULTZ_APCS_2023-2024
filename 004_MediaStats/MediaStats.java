db8b741f3a1ecae37827def71213952803704966
mschultz5832 <138723184+mschultz5832@users.noreply.github.com>
2023-09-19 11:31:47 -0500

    movie stats

---
+++ 004_MediaStats/MediaStats.java
@@ -0,0 +1,17 @@
+public class MediaStats
+{
+  public static void main(String[] args){
+    int hungerGamesReleaseYear = 2008;
+    int hungerGamesPageNumber = 374;
+    int onePieceaReleaseDate = 1999;
+    int onePieceRunTime = 24_051;
+    int theWalkingDeadReleaseDate = 2010;
+    int theWalkingDeadrunTime = 252;
+
+    System.out.println( "The hunger games was released in " + hungerGamesReleaseYear + " and it has " + hungerGamesPageNumber + " pages " + ".");
+    System.out.println( "One Piece was released in " + onePieceaReleaseDate + " and its run time is " + onePieceRunTime + " minutes ");
+    System.out.println( "The walking dead was released in " + theWalkingDeadReleaseDate + " and its run time is " + theWalkingDeadrunTime + " hours ");
+
+  }
+}
+
--- 00_helloworld/MediaStats.java
+++
@@ -1,5 +0,0 @@
-public class MediaStats
-{
-  public static void main(String[] args)
-  
-}
