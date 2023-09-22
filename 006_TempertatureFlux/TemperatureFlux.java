236b9891815654def855f9275d88e3d45543e500
mschultz5832 <138723184+mschultz5832@users.noreply.github.com>
2023-09-22 09:11:51 -0500

    adding numbers

--- 006_TempertatureFlux/TemperatureFlux.java
+++ 006_TempertatureFlux/TemperatureFlux.java
@@ -1,5 +1,11 @@
-package 006_TempertatureFlux;
-
 public class TemperatureFlux {
+
+    public static void main(String[ ] args){
+        int highestTemperatureOfTheDay = 79; 
+        int lowestTemperatureOfTheDay= 64; 
+        int differenceInTemperature = highestTemperatureOfTheDay - lowestTemperatureOfTheDay; 
+
+        System.out.println( " the highest temp for the day is " +  highestTemperatureOfTheDay +  " degrees "  +  " and the low for the day is " + lowestTemperatureOfTheDay +  " degrees " + " this makes the temperature difference " + differenceInTemperature );
     
+  }
 }
--- 007_SummationOfASEries/SummationOfASeries.java
+++ 007_SummationOfASEries/SummationOfASeries.java
@@ -1,5 +1,17 @@
-package 007_SummationOfASEries;
-
 public class SummationOfASeries {
-    
+
+    public static void main(String [] args){
+        int numberOne = 1;
+        int numberTwo = 2;
+        int numberThree = 3;
+        int numberfour = 4;
+        int numberFIve = 5; 
+        int numberSix = 6;
+        int numberSeven = 7;
+        int numberEight = 8;
+        int numberNine = 9;
+        int addittiveOfAllTheNumber = numberOne + numberTwo + numberThree + numberfour + numberFIve + numberSix + numberSeven + numberEight + numberNine; 
+
+ System.out.println( numberOne  + " + " + numberTwo + " + " + numberThree +  " + " + numberfour + " + " + numberFIve +  " + " + numberSix + " + " + numberSeven + " + " + numberEight + " + " + numberNine + " = " + addittiveOfAllTheNumber);
+}
 }
