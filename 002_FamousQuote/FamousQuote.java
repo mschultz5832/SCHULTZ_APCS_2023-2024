4a4a1fdf5552571d5682ff6494c65033d096c577
dgscdh <44707414+dgscdh@users.noreply.github.com>
2023-09-28 10:44:46 -0500

    Graded through 007.

--- 002_FamousQuote/FamousQuote.java
+++ 002_FamousQuote/FamousQuote.java
@@ -6,3 +6,12 @@ public class FamousQuote {
         System.out.println("\" - Mckenna Schultz \"");
     }
 }
+
+/*
+
+1/1 - Compiles
+1/1 - Content
+1/1 - Formatting
+1/1 - Comments
+
+*/
--- 003_MediaConcatenation/NameConcatenation.java
+++ 003_MediaConcatenation/NameConcatenation.java
@@ -5,3 +5,12 @@ public class NameConcatenation {
         System.out.println("My "  + "name " + "is " +  "Mckenna " +  "Schultz " + ".");
     }
  }
+
+/*
+
+1/1 - Compiles
+1/1 - Content
+1/1 - Formatting
+1/1 - Comments
+
+*/
