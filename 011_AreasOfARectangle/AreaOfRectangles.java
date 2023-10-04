e47a0e59ab8d2e0f21571877724713769e413490
mschultz5832 <138723184+mschultz5832@users.noreply.github.com>
2023-10-04 13:34:44 +0000

    fixed comment

--- 011_AreasOfARectangle/AreaOfRectangles.java
+++ 011_AreasOfARectangle/AreaOfRectangles.java
@@ -5,19 +5,19 @@ public class AreaOfRectangles {
 }
 AreasOfRectangles.java	"Write a program that calculates and prints the area of the following rectangles.
 
-Rec1AreaInInches:
--Width: 5.5 inches
--Height 7.8 inches
-Rec2AreaInCentimeters:
--Width: 99.4 centimeters
--Height: 500.352 centimeters
-Rec3AreaInCentimeters:
--Width: 75 inches
--Height: 800 centimeters
-Rec4AreaInInches:
--Width: 500029.75 inches
--Height: 90029384 centimeters
-Rec5AreaInYards:
--Width: 600000 centimeters
--Height: 8343491034 millimeters
+// Rec1AreaInInches:
+// -Width: 5.5 inches
+// -Height 7.8 inches
+// Rec2AreaInCentimeters:
+// -Width: 99.4 centimeters
+// -Height: 500.352 centimeters
+// Rec3AreaInCentimeters:
+// -Width: 75 inches
+// -Height: 800 centimeters
+// Rec4AreaInInches:
+// -Width: 500029.75 inches
+// -Height: 90029384 centimeters
+// Rec5AreaInYards:
+// -Width: 600000 centimeters
+// -Height: 8343491034 millimeters
 
