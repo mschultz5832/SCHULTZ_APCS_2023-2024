95c03ce38c2f151393e02efec691855714336bb5
dgscdh <44707414+dgscdh@users.noreply.github.com>
2023-10-09 09:42:41 -0500

    added 016-018 homework files

---
+++ 016_CarAge/ReturningCalculator.java
@@ -0,0 +1,43 @@
+public class ReturningCalculator
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public int integerAdder(int x, int y)
+    {
+        return x+y;
+    }
+
+    public double doubleAdder(double x, double y)
+    {
+        return x+y;
+    }
+    
+    public int integerSubtractor(int x, int y)
+    {
+        return x-y;
+    }
+
+    public double doubleSubtractor(double x, double y)
+    {
+        return x-y;
+    }
+
+    
+    public int integerMultiplier(int x, int y)
+    {
+        return x*y;
+    }
+
+    public double doubleMultiplier(double x, double y)
+    {
+        return x*y;
+    }
+
+    public double divider(double x, double y)
+    {
+        return x/y;
+    }
+}
