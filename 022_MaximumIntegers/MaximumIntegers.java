9b2cc75342f8457ffa7a56436cd05555374f9f13
mschultz5832 <mschultz5832@gmail.com>
2023-11-20 13:49:45 -0600

    hw

--- 022_MaximumIntegers/MaximumIntegers.java
+++ 022_MaximumIntegers/MaximumIntegers.java
@@ -1,5 +1,18 @@
-package 022_MaximumIntegers;
-
 public class MaximumIntegers {
+
+    public static void main(String[] args)
+   {
+     System.out.println(Integer.MIN_VALUE);
+     System.out.println(Integer.MAX_VALUE);
+     System.out.println(Integer.MIN_VALUE - );
+     System.out.println(Integer.MAX_VALUE + 1); 
     
+    int numberOne = Integer.MAX_VALUE = 1,000,000;
+
+    System.out.println(number1);
+
+	int numberTwo = Interger.MIN_VALUE = 745;
+
+    System.out.println( numberTwo)l;
+}
 }
