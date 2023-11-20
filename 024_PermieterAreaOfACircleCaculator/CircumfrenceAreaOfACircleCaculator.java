c79a813a571b066af7d6dd06577d3002ba6e76d1
mschultz5832 <mschultz5832@gmail.com>
2023-11-20 13:49:20 -0600

    hw

---
+++ 024_PermieterAreaOfACircleCaculator/CircumfrenceAreaOfACircleCaculator.java
@@ -0,0 +1,14 @@
+public class CircumfrenceAreaOfACircleCaculator{
+pulic static void main(String[] args){
+
+
+    Scanner scan = new Scanner(System.in);
+    double radiusOfCircle = 0.0;
+
+    System.out.println(" the radius of your circle is");
+    radius = scan.nextDouble
+
+    System.out.println( "area of circle is" + math.PI + Math.pow(radius, 2);
+
+}
+}
