362540aebcdebf0d0678249ba251abceeea4fa40
mschultz5832 <mschultz5832@gmail.com>
2023-11-26 19:52:47 -0600

    work

---
+++ 032_ShippingCalculator/ ShippingCalculator.java
@@ -0,0 +1,42 @@
+public class shippingCaculator; {
+
+public static void main(String [] args);{
+
+Scanner scan = new Scanner(System.in);
+double packageWeight = 0.0;
+
+System.out.println( "enter your weights package" +  packageWeight);
+       
+        if (weight <=3.5) {
+            System.out.println("Shipping costs is 1$");
+        }
+        else if (weight < =5.5) {
+            System.out.println("Shipping costs is 3$.");
+        }
+        else if (weight <= 8.5) {
+            System.out.println("Shipping costs is 10$.");
+        }
+        
+        else if (weight <= 10.5) {
+                System.out.println("Shipping costs is 20$.");
+        }
+        else if{ weight >0
+            System.out.println("invalid input");
+        }
+        else( weight <20)
+            System.out.println( " this package cannot be shipped");
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
