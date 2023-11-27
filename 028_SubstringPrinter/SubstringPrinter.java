fbae6eaac05d0f4560061d38579bf24b44c8b008
mschultz5832 <mschultz5832@gmail.com>
2023-11-26 19:35:03 -0600

    work

--- 028_SubstringPrinter/SubstringPrinter.java
+++ 028_SubstringPrinter/SubstringPrinter.java
@@ -1,1 +1,18 @@
-public static void main(String[] args);
+public class substringPrinter; {
+
+public static void main(String[] args);{
+
+    String sentence = "The quick brown fox jumps over the lazy dog.";
+     System.out.println(sentence.substring(32, 34));
+    // printing last word in sentence
+    System.out.println(sentence.substring(13, 15));
+    System.out.println(sentence.substring(30, 31));
+    System.out.println(sentence.substring(14, 18));
+    System.out.println(sentence.substring(34));
+
+}
+}
+
+// using subsrting to prit out certain words in stence counting from beginnning satrting at 0 
+
+
