8f0463966ecec742ac9b42f134467be17f90c23d
mschultz5832 <138723184+mschultz5832@users.noreply.github.com>
2024-01-18 14:11:50 -0600

    .

---
+++ 046_BasicNestedPractice/BasicNestedPractice.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class BasicNestedPractice {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+         System.out.println("Enter your character: ");
+        String userCharacter = scan.nextLine();
+
+         if (userCharacter.length() != 1) {
+            System.out.println("this does not work, please enter only a single character.");
+            // user must putin a single letter/number 
+        }
+        System.out.println("Enter the number of rows you want, ls than 20 : ");
+        int howManyRows = scan.nextInt();
+        // if its more than 1, it uses firts letter as charcter
+        
+       
+        System.out.print("Enter the number of columns you want, less than 20: ");
+        int howManyColumns = scan.nextInt();
+
+        for (int i = 0; i < howManyRows; i++) {
+            for (int x = 0; x < howManyColumns; x++) { 
+                System.out.print(userCharacter.substring(0, 1));
+            }
+            System.out.println(); 
+        }
+        scan.close();
+    }
+}
+
+// user inputs there characters, and it sues that withthe number of rows and colums they choose tomakeinto a table for them. 
