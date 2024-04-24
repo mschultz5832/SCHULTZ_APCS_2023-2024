9f265ecf5d024fe4ce426d130b07c4b3f5d9d921
mschultz5832 <138723184+mschultz5832@users.noreply.github.com>
2024-04-24 14:44:05 -0500

    Update WeatherComparison.java

--- 077_WeatherCompasrison/WeatherComparison.java
+++ 077_WeatherCompasrison/WeatherComparison.java
@@ -1,3 +1,24 @@
 public class WeatherComparison {
+
+    public static void main(String[] args) {
+
+        boolean[][] averageWeather = {
+            {"April 2015:", 60.25, 60.00, 39.92, 46.63, 47.75, 44.89, 41.38 },
+            {"April 2016:", 37.13, 33.91, 49.58, 35.46, 34.58, 41.09, 39.07 },
+            {"April 2017:", 48.00, 48.72, 48.73, 51.24, 48.83, 46.08, 47.17},
+            {"April 2018:", 25.08, 32.42, 31.67, 25.40 },
+            {"Alex","-----","Noah", "Jesse"},
+            {"-----","Liam","-----", "-----"},
+        };
+
+ 	
+//-Using the Wunderground History tool, create a 2D array of the daily average temperatures on April 1-7 for each year 2015-2020.
+-First, print out the average temperature of all 5 years combined.
+-Second, print out the average temperature for that week each year.
+-Third, print out the average temperature of each day across all years. In other words, average the April 1st temperature across 2019-2023.
+-I want 3 seperate methods for each average printing.
     
 }
+
+
+
